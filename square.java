8ed3d413cc34d5ce7d03d4bcdc2199a2b0a8d85b
happypattys <etzzsaguid@gmail.com>
2025-07-26 22:23:57 +0800

    swaure

---
+++ square.java
@@ -0,0 +1,34 @@
+import java.util.Scanner; // Import the Scanner class to get user input
+
+public class square {
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        
+        System.out.print("Enter an integer for the size of the square: ");
+
+        
+        int size = input.nextInt();
+
+        
+        if (size <= 0) {
+            System.out.println("Please enter a positive integer.");
+        } else {
+            System.out.println("Generating a square pattern of size " + size + ":");
+
+            
+            for (int i = 0; i < size; i++) {
+                
+                for (int j = 0; j < size; j++) {
+                    System.out.print("* "); 
+                }
+                System.out.println(); 
+            }
+        }
+
+        
+        input.close();
+    }
+}
